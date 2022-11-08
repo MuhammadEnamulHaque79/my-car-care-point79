1a2d2e163cc88f2327c04f550f45ebdcefca18be
Enamul <muhammad.enamul.haque79@gmail.com>
2022-11-08 04:39:46 -0800

    set environment varriable for firebase config

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './NotFound.css';
+import lazyMechanic from '../../../images/404/lazyMechanic.png';
 
 const NotFound = () => {
     return (
-        <div>
-            <h3 className='text-danger text-center'>The page NotFound!! 404!!</h3>
+        <div className='container'>
+            <h2 className='text-primary text-center'>Mechanic is sleeping!!</h2>
+            <img style={{display:'block',
+            marginLeft:'auto',
+            marginRight:'auto',
+            // width:'75%',
+            // border: '3px solid #73AD21',
+            // padding: '10px',
+            // borderRadius:'15px'
+        }} src={lazyMechanic} alt="" />
         </div>
     );
 };
