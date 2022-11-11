429370a7d709826a43273d315f7cee4e2f2bdf47
Enamul <muhammad.enamul.haque79@gmail.com>
2022-11-11 02:30:41 -0800

    updated

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -2,7 +2,11 @@ import React from 'react';
 import google from '../../../images/social/google.png';
 import facebook from '../../../images/social/facebook.png';
 import github from '../../../images/social/github.png';
-import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import {
+    useSignInWithFacebook,
+    useSignInWithGithub,
+    useSignInWithGoogle
+} from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import { useNavigate } from 'react-router-dom';
 import Loading from '../../Shared/Loading/Loading';
@@ -10,18 +14,20 @@ import Loading from '../../Shared/Loading/Loading';
 const SocialLogin = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
+    const [signInWithFacebook, user2, loading2, error2] = useSignInWithFacebook(auth);
+
     const navigate = useNavigate();
     let errorElement;
-    if(loading || loading1){
+    if (loading || loading1 || loading2) {
         return <Loading></Loading>
     }
-    if (error || error1) {
+    if (error || error1 || error2) {
         errorElement = <div>
-            <p className='text-danger'>Error: {error?.message} {error1?.message}</p>
+            <p className='text-danger'>Error: {error?.message} {error1?.message} {error2?.message}</p>
         </div>
     };
-    
-    if (user || user1) {
+
+    if (user || user1 || user2) {
         navigate('/home');
     }
     return (
@@ -41,7 +47,7 @@ const SocialLogin = () => {
                 </button>
 
                 <button
-
+                    onClick={()=>signInWithFacebook()}
                     style={{ borderRadius: '20px', text: 'center' }} className='btn btn-secondary w-50 mx-auto d-block mt-2'>
                     <img style={{ width: '30px' }} src={facebook} alt="" />
                     <span className='px-2'>CONTINUE WITH FACEBOOK</span>
@@ -60,6 +66,3 @@ const SocialLogin = () => {
 };
 
 export default SocialLogin;
-/*
-Facebook click handler not attached;
- */
